1a127c8cc15da0616a52daebf7dd891227924d82
Um11aut <manokhin.pavlo@lll.kpi.ua>
2023-12-24 17:35:44 +0100

    formatted

--- src/Node.hpp
+++ src/Node.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <memory>
 #include <iostream>
+#include <memory>
 
 class Node {
 public:
@@ -11,42 +11,45 @@ public:
   Node() : _val('\0') {}
 
   Node(const Node &other) : _val(other._val) {
-      _children.first = (other._children.first) ? std::make_unique<Node>(*other._children.first) : nullptr;
-      _children.second = (other._children.second) ? std::make_unique<Node>(*other._children.second) : nullptr;
+    _children.first = (other._children.first)
+                          ? std::make_unique<Node>(*other._children.first)
+                          : nullptr;
+    _children.second = (other._children.second)
+                           ? std::make_unique<Node>(*other._children.second)
+                           : nullptr;
   }
 
-  void setFirst(Ptr node) {
-      _children.first = std::move(node);
-  }
+  void setFirst(Ptr node) { _children.first = std::move(node); }
 
-  void setSecond(Ptr node) {
-      _children.second = std::move(node);
-  }
+  void setSecond(Ptr node) { _children.second = std::move(node); }
 
-  const inline char &get() const& { return _val; }
-  const inline _Pair &getPair() const& { return _children; }
+  const inline char &get() const & { return _val; }
+  const inline _Pair &getPair() const & { return _children; }
 
 private:
-    char _val;
-    _Pair _children = { nullptr, nullptr };
+  char _val;
+  _Pair _children = {nullptr, nullptr};
 };
 
 static void print(const Node::Ptr &node, int depth = 0) {
-    if (node.get() == nullptr) return;
+  if (node.get() == nullptr)
+    return;
 
-    for (int i = 0; i < depth; ++i) {
-        std::cout << "  ";
-    }
-    std::cout << node->get() << std::endl;
+  for (int i = 0; i < depth; ++i) {
+    std::cout << "  ";
+  }
+  std::cout << node->get() << std::endl;
 
-    print(node->getPair().first, depth + 1);
-    print(node->getPair().second, depth + 1);
+  print(node->getPair().first, depth + 1);
+  print(node->getPair().second, depth + 1);
 }
 
 static bool areEqual(const Node::Ptr &first, const Node::Ptr &second) {
-  if(first.get() == nullptr && second.get() == nullptr) return true;
+  if (first.get() == nullptr && second.get() == nullptr)
+    return true;
 
-  if(first->get() != second->get()) return false;
+  if (first->get() != second->get())
+    return false;
 
   areEqual(first->getPair().first, second->getPair().first);
   areEqual(first->getPair().second, second->getPair().second);
